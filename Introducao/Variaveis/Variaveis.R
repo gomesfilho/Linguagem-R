2d330c9e3e0cbc4e081e6dcedfaf72ad9f9be190
gomesfilho <gomesfilho@globo.com>
2020-01-24 16:22:16 -0300

    Atualização da pasta Variaveis

--- Introducao/Variaveis/Variaveis.R
+++ Introducao/Variaveis/Variaveis.R
@@ -26,4 +26,32 @@ help("typeof")
 
 # Uma variável pode ser uma lista de elementos
 
+var3 = c("primeiro","segundo","terceiro")
+var3
+mode(var3)
+
+#Uma variável pode ser uma função
+
+var4 = function(x){x + 3}
+var4
+mode(var4)
+
+# Podemos mudar também o modo do dado
+
+var5 = as.character(var1)
+var5
+mode(var5)
+
+# Atribuindo valores a objetos
+
+x <- c(1,2,3)
+x
+x1 = c(1,2,3)
+x1
+c(1,2,3) -> y
+y
+assign("x", c(6.3,4,-2))
+x
+
+
 
